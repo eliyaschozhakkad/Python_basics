b196f581ac13a27a452144e8f1ca36e662373a0a
eliyaschozhakkad <117439344+eliyaschozhakkad@users.noreply.github.com>
2023-04-19 06:58:58 +0530

    Add files via upload

---
+++ employee.py
@@ -0,0 +1,33 @@
+import logging
+import saveToFile as STF
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+f = logging.Formatter('%(levelname)s:%(message)s')
+
+fh = logging.FileHandler('employee.log')
+fh.setFormatter(f)
+
+logger.addHandler(fh)
+
+stream_handler = logging.StreamHandler()
+
+logger.addHandler(stream_handler)
+stream_handler.setFormatter(f)
+
+
+#logging.basicConfig(filename = 'employee.log', level = logger.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s')
+
+logger.debug('Start of employee program')
+name = 'nuh'
+age = 29
+email = 'elyas123@gmail.com'
+
+if STF.namecheck(name) is True:
+    STF.saveData(name, age, email)
+else:
+    logger.critical('Employee check failed')
+
+logger.debug('End of employee program')
+logger.debug('########################')
